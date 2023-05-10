455cc0ba36d726e51750b467b4a3cd11686521b7
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-05-10 20:58:07 +0300

    11

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store} basename="/goit-react-hw-05-movies">
+    <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
